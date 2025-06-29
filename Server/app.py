5f8bf96349ca41391ab0a7ccb910c99c539e7a70
Sahil Patel <sahilpatel@Sahils-Air-2.lan>
2025-06-28 22:19:31 -0400

    testing route id part 3

--- Server/app.py
+++ Server/app.py
@@ -14,25 +14,34 @@ def next_q_train():
     response = requests.get(FEED_URL)
     feed.ParseFromString(response.content)
 
-    arrival_times = []
+    q_arrival_times = []
+    b_arrival_times = []
 
     for entity in feed.entity:
         if not entity.HasField("trip_update"):
             continue
-        if entity.trip_update.trip.route_id != "Q":
+        if entity.trip_update.trip.route_id != "Q" or "B":
             continue
         for stop_time_update in entity.trip_update.stop_time_update:
             if stop_time_update.stop_id == DEKALB_Q_MANHATTAN_STOP_ID and "Q" in entity.trip_update.trip.route_id:
                 print(entity.trip_update.trip.route_id)
                 arrival_timestamp = stop_time_update.arrival.time
-                arrival_times.append(arrival_timestamp)
+                q_arrival_times.append(arrival_timestamp)
+            elif stop_time_update.stop_id == DEKALB_Q_MANHATTAN_STOP_ID and "B" in entity.trip_update.trip.route_id:
+                print(entity.trip_update.trip.route_id)
+                arrival_timestamp = stop_time_update.arrival.time
+                b_arrival_times.append(arrival_timestamp)
 
-    if arrival_times:
-        arrival_times.sort()
-        minutes_away = int((arrival_times[0] - time()) / 60)
+    if q_arrival_times or b_arrival_times:
+        q_arrival_times.sort()
+        q_minutes_away = int((q_arrival_times[0] - time()) / 60)
+        b_arrival_times.sort()
+        b_minutes_away = int((b_arrival_times[0] - time()) / 60)
         return jsonify({
-            "next_arrival_unix": arrival_times[0],
-            "minutes_away": minutes_away
+            "q_next_arrival_unix": q_arrival_times[0],
+            "q_minutes_away": q_minutes_away,
+            "b_next_arrival_unix": q_arrival_times[0],
+            "b_minutes_away": b_minutes_away
         })
     else:
         return jsonify({"error": "No arrivals found"}), 404
