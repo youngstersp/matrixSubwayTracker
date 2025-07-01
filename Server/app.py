e4eaa686af89fef897e9c1abad31e56344b89674
Sahil Patel <sahilpatel@Sahils-Air-2.lan>
2025-06-30 21:06:38 -0400

    troubleshooting b train stop times part 2

--- Server/app.py
+++ Server/app.py
@@ -5,19 +5,24 @@ from time import time
 
 app = Flask(__name__)
 
-FEED_URL = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw"
+Q_FEED_URL = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw"
+B_FEED_URL = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm"
 DEKALB_Q_MANHATTAN_STOP_ID = "R30N"
 
 @app.route("/next_train")
 def next_q_train():
-    feed = gtfs_realtime_pb2.FeedMessage()
-    response = requests.get(FEED_URL)
-    feed.ParseFromString(response.content)
+    qfeed = gtfs_realtime_pb2.FeedMessage()
+    qresponse = requests.get(Q_FEED_URL)
+    qfeed.ParseFromString(qresponse.content)
+
+    bfeed = gtfs_realtime_pb2.FeedMessage()
+    bresponse = requests.get(B_FEED_URL)
+    bfeed.ParseFromString(bresponse.content)
 
     q_arrival_times = []
     b_arrival_times = []
 
-    for entity in feed.entity:
+    for entity in qfeed.entity:
         if not entity.HasField("trip_update"):
             continue
         for stop_time_update in entity.trip_update.stop_time_update:
@@ -29,6 +34,14 @@ def next_q_train():
             elif stop_time_update.stop_id == DEKALB_Q_MANHATTAN_STOP_ID and "B" in entity.trip_update.trip.route_id:
                 arrival_timestamp = stop_time_update.arrival.time
                 b_arrival_times.append(arrival_timestamp)
+    for entity in bfeed.entity:
+        if not entity.HasField("trip_update"):
+            continue
+        for stop_time_update in entity.trip_update.stop_time_update:
+            if stop_time_update.stop_id == DEKALB_Q_MANHATTAN_STOP_ID and "B" in entity.trip_update.trip.route_id:
+                arrival_timestamp = stop_time_update.arrival.time
+                b_arrival_times.append(arrival_timestamp)
+
     if q_arrival_times:
         b_arrive_time = -1;
         q_arrive_time = -1;
